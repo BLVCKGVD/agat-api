5b824f21adc067bb6aa4a3c3a2695a2c5b8597fc
BLVCKGVD <antondead1337@gmail.com>
2022-04-21 09:40:12 +0300

    email sheesh

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Tetranz\Select2EntityBundle\TetranzSelect2EntityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
