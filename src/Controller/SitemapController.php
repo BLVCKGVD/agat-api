1e36a252748ba287790f5e50972ab9cc0864df5a
BLVCKGVD <antondead1337@gmail.com>
2022-05-02 23:01:13 +0300

    email sheesh

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -11,7 +11,35 @@ class SitemapController extends AbstractController
 
     public function index(): Response
     {
-        return $this->render("sitemap.xml");
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+
+        <loc>https://www.agat-avia.ru/page/main_page</loc>
+
+        <lastmod>2022-05-02</lastmod>
+
+        <priority>0.8</priority>
+
+    </url>
+
+
+    <url>
+
+        <loc>https://www.agat-avia.ru/page/contacts</loc>
+
+        <lastmod>2022-05-02</lastmod>
+
+    </url>
+
+
+</urlset>
+';
+        $response = new Response($xml);
+        $response->headers->set('Content-Type', 'xml');
+        return $response;
     }
 
 }
